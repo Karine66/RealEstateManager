db02d1a21237a55bccda9c650bcf4d081e9ce711
Karine66 <shon@live.fr>
2020-06-17 16:51:13 +0200

    set up layout recyclerview for listFragment

--- app/src/main/java/com/openclassrooms/realestatemanager/listPage/ListFragment.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/listPage/ListFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.openclassrooms.realestatemanager.R;
+import com.openclassrooms.realestatemanager.databinding.FragmentListBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +22,8 @@ public class ListFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private FragmentListBinding fragmentListBinding;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -60,7 +63,15 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        // For viewBinding
+        fragmentListBinding = FragmentListBinding.inflate(inflater, container,false);
+        View view = fragmentListBinding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        fragmentListBinding = null;
     }
 }
