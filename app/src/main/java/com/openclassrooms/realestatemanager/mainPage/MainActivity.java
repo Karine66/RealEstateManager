424ea1390a7087ba8814117b312d0e071941846b
Karine66 <shon@live.fr>
2020-06-17 15:26:30 +0200

    set up viewBinder + attach fragments to activity

--- app/src/main/java/com/openclassrooms/realestatemanager/mainPage/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/mainPage/MainActivity.java
@@ -4,41 +4,68 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.openclassrooms.realestatemanager.R;
+import com.openclassrooms.realestatemanager.databinding.ActivityMainBinding;
+import com.openclassrooms.realestatemanager.detailDescription.DetailFragment;
+import com.openclassrooms.realestatemanager.listPage.ListFragment;
 import com.openclassrooms.realestatemanager.utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textViewMain;
-    private TextView textViewQuantity;
+    private ActivityMainBinding binding;
+    private ListFragment listFragment;
+    private DetailFragment detailFragment;
 
-    @Override
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //for view binding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+//        //First error id call error
+////        this.textViewMain = findViewById(R.id.activity_second_activity_text_view_main);
+         this.configureAndShowListFragment();
+         this.configureAndShowDetailFragment();
+         }
 
-        //First error id call error
-//        this.textViewMain = findViewById(R.id.activity_second_activity_text_view_main);
-        this.textViewMain = findViewById(R.id.activity_main_activity_text_view_main);
-        this.textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity);
+//    @SuppressLint("SetTextI18n")
+//    private void configureTextViewQuantity(){
+//        int quantity = Utils.convertDollarToEuro(100);
+//        this.textViewQuantity.setTextSize(20);
+//        //Second Error : call integer in setText
+//        //this.texViewQuantity.setText(quantity);
+//        this.textViewQuantity.setText(Integer.toString(quantity));
+//    }
 
-        this.configureTextViewMain();
-        this.configureTextViewQuantity();
+    private void configureAndShowListFragment(){
+        //Get FragmentManager (Support) and Try to find existing instance of fragment in FrameLayout container
+        listFragment = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.frame_layout_main);
+
+        if (listFragment == null) {
+            //Create new main fragment
+            listFragment = new ListFragment();
+            //Add it to FrameLayout container
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frame_layout_main, listFragment)
+                    .commit();
+        }
     }
 
-    private void configureTextViewMain(){
-        this.textViewMain.setTextSize(15);
-        this.textViewMain.setText("Le premier bien immobilier enregistré vaut ");
-    }
+    private void configureAndShowDetailFragment(){
+        //Get FragmentManager (Support) and Try to find existing instance of fragment in FrameLayout container
+        detailFragment = (DetailFragment) getSupportFragmentManager().findFragmentById(R.id.frame_layout_main);
 
-    @SuppressLint("SetTextI18n")
-    private void configureTextViewQuantity(){
-        int quantity = Utils.convertDollarToEuro(100);
-        this.textViewQuantity.setTextSize(20);
-        //Second Error : call integer in setText
-        //this.texViewQuantity.setText(quantity);
-        this.textViewQuantity.setText(Integer.toString(quantity));
+        if (detailFragment == null) {
+            //Create new main fragment
+            detailFragment = new DetailFragment();
+            //Add it to FrameLayout container
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frame_layout_main, detailFragment)
+                    .commit();
+        }
     }
 }
