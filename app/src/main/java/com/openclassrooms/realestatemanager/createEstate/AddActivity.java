17baa49d50e814071c6d31f9ae324281c327e4ba
Karine66 <shon@live.fr>
2020-06-22 15:08:39 +0200

    start of create estate form layout

--- app/src/main/java/com/openclassrooms/realestatemanager/createEstate/AddActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/createEstate/AddActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.ActionBar;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import com.openclassrooms.realestatemanager.BaseActivity;
+import com.openclassrooms.realestatemanager.R;
 import com.openclassrooms.realestatemanager.databinding.ActivityAddBinding;
 
 import java.util.Objects;
@@ -21,13 +24,30 @@ public class AddActivity extends BaseActivity {
         activityAddBinding = ActivityAddBinding.inflate(getLayoutInflater());
         View view = activityAddBinding.getRoot();
         setContentView(view);
+
+        //For toolbar
         this.configureToolbar();
         this.configureUpButton();
-
         //for title toolbar
         ActionBar ab = getSupportActionBar();
-       Objects.requireNonNull(ab).setTitle("Create Estate");
+        Objects.requireNonNull(ab).setTitle("Create Estate");
+
+        this.dropDownEstates(view);
+
+
 
     }
+    //For dropDown Estates
+    public void dropDownEstates (View view) {
+
+        String [] ESTATES = new String[] {"House", "Flat", "Duplex", "Penthouse"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.dropdown_menu_popup_item, ESTATES);
+
+        AutoCompleteTextView editTextOutlinedExposedDropdown = view.findViewById(R.id.et_Estate);
+        editTextOutlinedExposedDropdown.setAdapter(adapter);
+    }
+
+
+
 
 }
