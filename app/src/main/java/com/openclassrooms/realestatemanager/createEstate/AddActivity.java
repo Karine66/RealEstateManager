562b41e749990d998a39ff1e4065c5d917345317
Karine66 <shon@live.fr>
2020-06-27 21:04:59 +0200

    dropdown generic and datepicker

--- app/src/main/java/com/openclassrooms/realestatemanager/createEstate/AddActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/createEstate/AddActivity.java
@@ -1,22 +1,24 @@
 package com.openclassrooms.realestatemanager.createEstate;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.fragment.app.DialogFragment;
 
-import android.content.Context;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.BaseAdapter;
+import android.widget.DatePicker;
 
 import com.openclassrooms.realestatemanager.BaseActivity;
 import com.openclassrooms.realestatemanager.R;
 import com.openclassrooms.realestatemanager.databinding.ActivityAddBinding;
 
-import java.util.Arrays;
+import java.text.DateFormat;
+import java.util.Calendar;
 import java.util.Objects;
 
-public class AddActivity extends BaseActivity {
+public class AddActivity extends BaseActivity implements DatePickerDialog.OnDateSetListener, View.OnClickListener{
 
     private ActivityAddBinding activityAddBinding;
 
@@ -42,87 +44,53 @@ public class AddActivity extends BaseActivity {
         ActionBar ab = getSupportActionBar();
         Objects.requireNonNull(ab).setTitle("Create Estate");
 
-//        this.dropDownString();
-        this.dropDownView(view);
         this.dropDownAdapters();
 
-//        this.dropDownEstates(view);
-//        this.dropDownRooms(view);
-//        this.dropDownBedrooms(view);
-//        this.dropDownBathrooms(view);
-//        this.dropDownAgentName(view);
+        activityAddBinding.upOfSaleDate.setOnClickListener(this);
+        activityAddBinding.soldDate.setOnClickListener(this);
+
 
     }
-
-
+    //for adapter generic
+    private ArrayAdapter<String> factoryAdapter(int resId) {
+        return new ArrayAdapter<>(this, R.layout.dropdown_menu_popup_item, getResources().getStringArray(resId));
+    }
 
     //For adapters dropdown
     public void dropDownAdapters() {
 
-        ArrayAdapter <String> adapterEstates = new ArrayAdapter<>(this, R.layout.dropdown_menu_popup_item,getResources().getStringArray(R.array.ESTATES));
-        ArrayAdapter<String> adapterRooms = new ArrayAdapter<>(this, R.layout.dropdown_menu_popup_item, getResources().getStringArray(R.array.ROOMS));
-        ArrayAdapter<String> adapterBedrooms = new ArrayAdapter<>(this, R.layout.dropdown_menu_popup_item, getResources().getStringArray(R.array.BEDROOMS));
-        ArrayAdapter<String> adapterBathrooms = new ArrayAdapter<>(this, R.layout.dropdown_menu_popup_item, getResources().getStringArray(R.array.BATHROOMS));
-        ArrayAdapter<String> adapterAgent = new ArrayAdapter<>(this, R.layout.dropdown_menu_popup_item, getResources().getStringArray(R.array.AGENT));
-        dropdownEstate.setAdapter(adapterEstates);
-        dropdownRooms.setAdapter(adapterRooms);
-        dropdownBedrooms.setAdapter(adapterBedrooms);
-        dropdownBathrooms.setAdapter(adapterBathrooms);
-        dropdownAgent.setAdapter(adapterAgent);
-    }
-    //For view dropdown
-    public void dropDownView(View view) {
-       dropdownEstate = view.findViewById(R.id.et_Estate);
-       dropdownRooms = view.findViewById(R.id.et_rooms);
-       dropdownBedrooms = view.findViewById(R.id.et_bedrooms);
-       dropdownBathrooms = view.findViewById(R.id.et_bathrooms);
-       dropdownAgent = view.findViewById(R.id.et_agent);
-    }
-    //For dropDown Estates
-    public void dropDownEstates (View view) {
-
-         String [] ESTATES = new String[] {"House", "Flat", "Duplex","Penthouse"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.dropdown_menu_popup_item, ESTATES);
-         AutoCompleteTextView editTextOutlinedExposedDropdown = view.findViewById(R.id.et_Estate);
-
-        editTextOutlinedExposedDropdown.setAdapter(adapter);
+        activityAddBinding.etEstate.setAdapter(factoryAdapter(R.array.ESTATES));
+        activityAddBinding.etRooms.setAdapter(factoryAdapter(R.array.ROOMS));
+        activityAddBinding.etBedrooms.setAdapter(factoryAdapter(R.array.BEDROOMS));
+        activityAddBinding.etBathrooms.setAdapter(factoryAdapter(R.array.BATHROOMS));
+        activityAddBinding.etAgent.setAdapter(factoryAdapter(R.array.AGENT));
     }
 
-    //For dropDown number of rooms
-    public void dropDownRooms (View view) {
+    public void onClick (View view) {
+        switch (view.getId()) {
+            case R.id.upOfSaleDate:
+            case R.id.soldDate:
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
+                break;
 
-        String [] ROOMS = new String[] {"1", "2", "3","4","5 et +"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.dropdown_menu_popup_item, ROOMS);
-
-        AutoCompleteTextView editTextOutlinedExposedDropdown = view.findViewById(R.id.et_rooms);
-        editTextOutlinedExposedDropdown.setAdapter(adapter);
+        }
     }
-    //For dropDown number of bedrooms
-    public void dropDownBedrooms (View view) {
 
-        String [] BEDROOMS = new String[] {"0","1", "2", "3","4","5 et +"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.dropdown_menu_popup_item, BEDROOMS);
 
-        AutoCompleteTextView editTextOutlinedExposedDropdown = view.findViewById(R.id.et_bedrooms);
-        editTextOutlinedExposedDropdown.setAdapter(adapter);
-    }
-    //For dropDown number of bathrooms
-    public void dropDownBathrooms (View view) {
 
-        String [] BATHROOMS = new String[] {"1", "2", "3","4 et +"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.dropdown_menu_popup_item, BATHROOMS);
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
 
-        AutoCompleteTextView editTextOutlinedExposedDropdown = view.findViewById(R.id.et_bathrooms);
-        editTextOutlinedExposedDropdown.setAdapter(adapter);
-    }
-    //For dropDown real estate agent name
-    public void dropDownAgentName (View view) {
 
-        String [] AGENT = new String[] {"Karine Danjard", "John Doe", "Octave Dupont","Eugene Martin"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.dropdown_menu_popup_item, AGENT);
-
-        AutoCompleteTextView editTextOutlinedExposedDropdown = view.findViewById(R.id.et_agent);
-        editTextOutlinedExposedDropdown.setAdapter(adapter);
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance(DateFormat.DATE_FIELD).format(c.getTime());
+        String currentDateStringSold = DateFormat.getDateInstance(DateFormat.DATE_FIELD).format(c.getTime());
+        activityAddBinding.upOfSaleDate.setText(currentDateString);
+        activityAddBinding.soldDate.setText(currentDateStringSold);
     }
 
 }
