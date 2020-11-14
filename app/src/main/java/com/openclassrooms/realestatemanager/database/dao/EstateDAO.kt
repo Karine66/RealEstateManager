82a1cb93603b140792c55e17df3b0f551d5ccd5f
Karine66 <shon@live.fr>
2020-11-14 13:53:43 +0100

    video ok

--- app/src/main/java/com/openclassrooms/realestatemanager/database/dao/EstateDAO.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/database/dao/EstateDAO.kt
@@ -3,34 +3,33 @@ package com.openclassrooms.realestatemanager.database.dao
 import android.database.Cursor
 import androidx.lifecycle.LiveData
 import androidx.room.*
-import androidx.sqlite.db.SupportSQLiteDatabase
 import androidx.sqlite.db.SupportSQLiteQuery
 import com.openclassrooms.realestatemanager.models.Estate
-import com.openclassrooms.realestatemanager.models.SearchEstate
 
 @Dao
 interface EstateDAO {
 
 
-        @Query("SELECT * FROM Estate")
-        fun getEstates(): LiveData<List<Estate>>
+    @Query("SELECT * FROM Estate")
+    fun getEstates(): LiveData<List<Estate>>
 
-        @Query("SELECT * FROM Estate WHERE mandateNumberID = :mandateNumberID")
-        fun getEstate(mandateNumberID: Long): LiveData<Estate>
+    @Query("SELECT * FROM Estate WHERE mandateNumberID = :mandateNumberID")
+    fun getEstate(mandateNumberID: Long): LiveData<Estate>
 
-        @Insert
-        fun insertEstate(estate: Estate):Long
-        @Update
-        fun updateEstate(estate: Estate):Int
+    @Insert
+    fun insertEstate(estate: Estate): Long
 
-        @Query("DELETE FROM Estate WHERE mandateNumberID = :mandateNumberID")
-        fun deleteItem(mandateNumberID:Long):Int
+    @Update
+    fun updateEstate(estate: Estate): Int
 
-       @RawQuery(observedEntities = [Estate::class])
-       fun getSearchEstate(query: SupportSQLiteQuery) : LiveData<List<Estate>>
+    @Query("DELETE FROM Estate WHERE mandateNumberID = :mandateNumberID")
+    fun deleteItem(mandateNumberID: Long): Int
+
+    @RawQuery(observedEntities = [Estate::class])
+    fun getSearchEstate(query: SupportSQLiteQuery): LiveData<List<Estate>>
 
     //For ContentProvider
     @Query("SELECT * FROM Estate WHERE mandateNumberID = :mandateNumberID")
-    fun getEstateWithCursor(mandateNumberID: Long):Cursor
+    fun getEstateWithCursor(mandateNumberID: Long): Cursor
 
-    }
+}
